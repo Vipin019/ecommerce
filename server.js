704512100ce69008afa42e94aa5560c2453bc287
Vipin019 <vkpmail019@gmail0com>
2023-03-21 13:19:18 +0530

    for deploy at render

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
 
 //configure env
 dotenv.config();
@@ -21,11 +22,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes);
+app.use("*", function (req, res) {
+  res.sendFile(__dirname, path.join("./client/build/index.html"));
+});
+// app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/category", categoryRoutes);
+// app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.get("/", (req, res) => {
